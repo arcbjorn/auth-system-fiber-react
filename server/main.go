2ba2b465fc05c43965b54959a22b06b218dd6555
arcbjorn <arky.happ@gmail.com>
2021-05-13 22:28:59 +0300

    feat (server): add main entrypoint

---
+++ server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"server/database"
+	"server/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	database.Connect()
+
+	app := fiber.New()
+
+	// Authentication with HttpOnly
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	routes.Setup(app)
+
+	app.Listen(":8000")
+}
